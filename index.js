d761052c520e94611a722c8613ae0d9902bda85e
Slarti @ DURY LEGAL <slarti@dury.de>
2026-05-03 22:38:26 +0200

    Initial commit: index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+// ip-automation - IP automation toolkit - Automate intellectual property workflows. Powered by DURY LEGAL.
+// Coming soon. Visit https://www.dury.de for more information.
+module.exports = {};
